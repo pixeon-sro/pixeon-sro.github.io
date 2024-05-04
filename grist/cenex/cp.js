7bc50840370a4e82157081ac60ad9cd07166ab89
pixeon-sro <box@pixeon.sk>
2024-05-04 06:56:48 +0200

    develop

--- grist/cenex/cp.js
+++ grist/cenex/cp.js
@@ -1,7 +1,13 @@
+/*
+async function example() {
+  return await grist.docApi.fetchSelectedTable();
+}
+*/
+
 // grist pozaduje pristu na čítanie tabulky
 grist.ready({ requiredAccess: 'read' })
 
-grist.docApi.fetchSelectedTable.then((resp) => {
+grist.docApi.fetchSelectedTable(then(resp) => {
             myDisplay(resp)
       }
 );
