c50218832d6fd7bc52caf0487056d985a14f24cc
pixeon-sro <box@pixeon.sk>
2024-05-06 09:46:32 +0200

    develop

--- grist/cenex/vykazCenovaPonuka/cp.js
+++ grist/cenex/vykazCenovaPonuka/cp.js
@@ -1,5 +1,5 @@
 //verzia scriptu do konzoly
-console.log("*** cp.js - ver: 027")
+console.log("*** cp.js - ver: 029")
       
 // grist požaduje plný prístup
 grist.ready({ requiredAccess: 'full' })
@@ -19,28 +19,35 @@ data.then(
 
 //vytvorenie oddelených objektov pre tlačové tabulky
 function tabData(value) {
-    const ref = value[0]
-    const tabMaterial = ref.References.Vykaz_Vymer_Material
-    const tabPraca = ref.References.Vykaz_Vymer_Praca
-    const tabNaklady = ref.References.Pridruzene_naklady
-    const tabCena = ref.References.Konecna_Cena
-    const etapa = ref.References.Etapa
+    let ref = value[0]
+    let material = ref.References.Vykaz_Vymer_Material
+    let praca = ref.References.Vykaz_Vymer_Praca
+    let naklady = ref.References.Pridruzene_naklady
+    let cena = ref.References.Konecna_Cena
+    let etapa = ref.References.Etapa
 
-    function vykazVymerMaterial(){return tabMaterial}
-    function vykazVymerPraca(){return tabPraca}
-    function vykazPridruzeneNaklady(){return tabNaklady}
-    function vykazKonecnaCena(){return tabCena}
-    function etapa(){return etapa}
+    function vykazVymerMaterial(){
+        let vykazVymerMaterial = []
+        forEach(Material) {
+        
+        }
+        
+        return vykazVymerMaterial
+    }
+    function vykazVymerPraca(){return praca}
+    function vykazPridruzeneNaklady(){return naklady}
+    function vykazKonecnaCena(){return cena}
+    function vykazEtapa(){return etapa}
 
     console.log("******")
     console.log(ref)
     console.log("******")
     console.log(ref.References)
     console.log("******")
-    console.log(tabMaterial)
-    console.log(tabPraca)
-    console.log(tabNaklady)
-    console.log(tabCena)
+    console.log(material)
+    console.log(praca)
+    console.log(naklady)
+    console.log(cena)
     console.log(etapa)
     console.log("******")
 }
