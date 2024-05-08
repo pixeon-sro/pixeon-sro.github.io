a140154f2ef7d169893e9c330ccda1a46db1f534
pixeon-sro <box@pixeon.sk>
2024-05-08 18:20:50 +0200

    develop

--- grist/cenex/vykazCenovaPonuka/dataCP.js
+++ grist/cenex/vykazCenovaPonuka/dataCP.js
@@ -88,8 +88,13 @@ Promise.allSettled(allPromises).then(function(data){
   console.log(tEtapa)
 
   // vytvorenie referencií z tCP
-  const vVMaterial = tCP[0].References
+  const vVMaterial = tCP[0].References.Vykaz_Vymer_Praca
   console.log(vVMaterial)
-  // zápis hlavičky CP
+  const vVPraca = tCP[0].References.Vykaz_Vymer_Práca
+  console.log(vVPraca)
+  const vVNaklady = tCP[0].References.Pridruzene_naklady
+  console.log(vVNaklady)
+  const vCelkovaCena = tCP[0].References.Konecna_Cena
+  console.log(vCelkovaCena)
 
 })
