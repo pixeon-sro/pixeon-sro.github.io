78d82635444c761c1fc0d9938693cae26a58b472
pixeon-sro <box@pixeon.sk>
2024-05-09 16:19:23 +0200

    uvod do cestaku
    uvod do cestaku

---
+++ grist/cestak/engine.js
@@ -0,0 +1,14 @@
+/*
+/
+/ Načítanie a príprava tlačových tabuliek z údajov tabuliek CESTAKU
+/
+/ autor: Roman Holinec
+/ box@pixeon.sk
+/ verzia: 001
+/
+*/
+
+console.log("*** Print teplate for CESTAK")
+console.log("*** cp.js - ver: 060")
+console.log("*** autor: Roman Holinec")
+console.log("*** mail: box@pixeon.sk")
