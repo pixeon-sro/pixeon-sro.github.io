cbbc3ae19a2f92ecc6d632547c1f1ed2a8c21e1f
pixeon-sro <box@pixeon.sk>
2024-05-09 09:57:37 +0200

    develop

--- grist/cenex/vykazCenovaPonuka/dataCP.js
+++ grist/cenex/vykazCenovaPonuka/dataCP.js
@@ -153,7 +153,7 @@ Promise.allSettled(allPromises).then(function(data){
     cellJednotka.innerText = item.jednotka
     cellJadnotkovCena.innerText = item.jednotkova_cena
     cellMnozstvo.innerText = item.mnozstvo
-    cellCelkovaCena.innerText = round(item.celkova_cena, 2)
+    cellCelkovaCena.innerText = item.celkova_cena.toFixed(2)
   })
 
   // vytvorenie tlačovej tabuľky výkazu Práce
@@ -198,7 +198,7 @@ Promise.allSettled(allPromises).then(function(data){
     cellJednotka.innerText = item.jednotka
     cellJadnotkovCena.innerText = item.jednotkova_cena
     cellMnozstvo.innerText = item.mnozstvo
-    cellCelkovaCena.innerText = round(item.celkova_cena, 2)
+    cellCelkovaCena.innerText = item.celkova_cena.toFixed(2)
   })
 
   // vytvorenie tlačovej tabuľky výkazu Pridružených nákladov
@@ -246,7 +246,7 @@ Promise.allSettled(allPromises).then(function(data){
     cellJednotka.innerText = item.jednotka
     cellJadnotkovCena.innerText = item.jednotkova_cena
     cellMnozstvo.innerText = item.mnozstvo
-    cellCelkovaCena.innerText = round(item.celkova_cena, 2)
+    cellCelkovaCena.innerText = item.celkova_cena.toFixed(2)
   })
 
   // vypísanie Celkovej ceny
@@ -260,10 +260,10 @@ Promise.allSettled(allPromises).then(function(data){
     let cellCelkovaCena = tRow.insertCell(4)
 
     cellPolozka.innerText = item.polozka
-    cellMaterial.innerText =  round(item.material, 2)
-    cellPraca.innerText =  round(item.praca, 2)
-    cellNaklady.innerText =  round(item.pridruzene_naklady, 2)
-    cellCelkovaCena.innerText =  round(item.celkova_cena, 2)
+    cellMaterial.innerText = item.material.toFixed(2)
+    cellPraca.innerText = item.praca.toFixed(2)
+    cellNaklady.innerText = item.pridruzene_naklady.toFixed(2)
+    cellCelkovaCena.innerText = item.celkova_cena.toFixed(2)
   })
 
 }) //ukončenie Promise.allSettled
