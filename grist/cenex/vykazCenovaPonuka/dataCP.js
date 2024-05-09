63030e72efebffdf10c7500fdd1ec7a456a90d63
pixeon-sro <box@pixeon.sk>
2024-05-09 09:02:39 +0200

    develop

--- grist/cenex/vykazCenovaPonuka/dataCP.js
+++ grist/cenex/vykazCenovaPonuka/dataCP.js
@@ -66,7 +66,7 @@ function convertor(value) {
 // zaokruhlovanie čísel
 
 function round(num, decimal=0) {
-  Math.round((num  * 10 ** decimal) * (1 + Number.EPSILON)) /  10 ** decimal
+  return Math.round((num  * 10 ** decimal) * (1 + Number.EPSILON)) /  10 ** decimal
 }
 
 // spracovanie údajov pre tlač
