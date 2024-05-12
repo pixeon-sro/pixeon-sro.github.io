454568871caa110d4a6d3c23fa48c5204f28799c
pixeon-sro <box@pixeon.sk>
2024-05-12 07:36:35 +0200

    develop

--- grist/cestak/engine.js
+++ grist/cestak/engine.js
@@ -1,6 +1,6 @@
 /*
 /
-/ Načítanie a príprava tlačových tabuliek z údajov tabuliek CESTAKU
+/ Načítanie a príprava tlačových údajov pre CESŤÁK
 /
 / autor: Roman Holinec
 / box@pixeon.sk
@@ -9,6 +9,17 @@
 */
 
 console.log("*** Print teplate for CESTAK")
-console.log("*** cp.js - ver: 060")
+console.log("*** engine.js - ver: 001")
 console.log("*** autor: Roman Holinec")
 console.log("*** mail: box@pixeon.sk")
+
+// grist požaduje plný prístup
+grist.ready({ requiredAccess: 'full' })
+
+// načítanie údajov z Cestaku
+let dbCestak = dbCestak()
+async function dbCestak() {
+    let dataFromCestak = await grist.docApi.fetchSelectedTable(options = {format:"rows"})
+    return dataFromCestak
+}
+console.log(dbCestak)
