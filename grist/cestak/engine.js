b0a71ef1141a8604772a594bc4d684886b53f172
pixeon-sro <box@pixeon.sk>
2024-05-12 07:44:56 +0200

    dev Cestak

--- grist/cestak/engine.js
+++ grist/cestak/engine.js
@@ -22,4 +22,20 @@ async function dbCestak() {
     let dataFromCestak = await grist.docApi.fetchSelectedTable(options = {format:"rows"})
     return dataFromCestak
 }
-console.log(dbCestak)
+
+// zaokruhlovanie čísel
+function round(num, decimal=0) {
+  return Math.round((num  * 10 ** decimal) * (1 + Number.EPSILON)) /  10 ** decimal
+}
+
+//  pole všetkých Promisov
+allPromises = [
+  dbCestak
+]
+
+Promise.allSettled(allPromises).then(function(data){
+
+  const tCestak = data
+  console.log(tCestak)
+
+}) //ukončenie Promise.allSettled
