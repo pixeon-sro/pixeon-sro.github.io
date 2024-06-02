a686f59b2a02a217236eea75281c06d41077a5d7
pixeon-sro <box@pixeon.sk>
2024-06-02 06:22:00 +0200

    develop

--- grist/cestak/engine.js
+++ grist/cestak/engine.js
@@ -238,7 +238,7 @@ Promise.allSettled(allPromises).then(function(data) {
     let cellPhmH=tRowB.insertCell(0)
     let cellPhmV=tRowB.insertCell(1)
     cellPhmH.innerHTML="Celkové náhrady za PHM"
-    cellPhmV.innerHTML=sumaNahradaPHM + "€"
+    cellPhmV.innerHTML=round(sumaNahradaPHM, 2) + "€"
     let cellPreddavokH=tRowB.insertCell(2)
     let cellPreddavokV=tRowB.insertCell(3)
     cellPreddavokH.innerHTML="Preddavok:"
@@ -252,7 +252,7 @@ Promise.allSettled(allPromises).then(function(data) {
     let cellStravaH=tRowC.insertCell(0)
     let cellStravaV=tRowC.insertCell(1)
     cellStravaH.innerHTML="Celkové náhrady za stravu"
-    cellStravaV.innerHTML=sumaNahradaStrava + "€"
+    cellStravaV.innerHTML=round(sumaNahradaStrava, 2) + "€"
     let cellDoplatokH=tRowC.insertCell(2)
     let cellDoplatokV=tRowC.insertCell(3)
     cellDoplatokH.innerHTML="Doplatok:"
