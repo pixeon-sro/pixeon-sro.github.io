647f67c2808fe6f590c49cda7ce129f57d39a8f3
pixeon-sro <box@pixeon.sk>
2024-06-24 16:25:50 +0200

    develop

--- grist/cenex/narezPlan/dataNP.js
+++ grist/cenex/narezPlan/dataNP.js
@@ -13,6 +13,37 @@ console.log("*** dataNP.js - ver: 001")
 console.log("*** autor: Roman Holinec")
 console.log("*** mail: box@pixeon.sk")
 
+// zaokruhlovanie čísel
+function round(num, decimal=0) {
+  return Math.round((num  * 10 ** decimal) * (1 + Number.EPSILON)) /  10 ** decimal
+}
+// zitovanie prazdneho poľa
+// ak je pole prazdne, vráti TRUE
+// ak pole obsahuje prvky, vráti FALSE
+function isEmpty(value) {
+  if (value instanceof Array) {
+    if (value.legth === 0) {
+      return true
+    }
+    else {
+      return false
+    }
+  }
+  else if (value instanceof Object) {
+    if (value.legth === 0) {
+      return true
+    }
+    else {
+      return false
+    }
+  }
+  else {
+    console.log("this is not Array or Object")
+    console.log(value)
+    return true
+  }
+}
+
 // grist požaduje plný prístup
 grist.ready({ requiredAccess: 'full' })
 
@@ -31,8 +62,15 @@ allPromises = [
 
 // načítanie všetkych Promisov a príprava polí objektov
 Promise.allSettled(allPromises).then(function(data){
+  console.log(data)
+  /*
+  /  ak existuje tabuľka Nárezový plán a
+  /  obsahuje data, vytvoria sa údaje pre tlač
+  */
+  if (!isEmpty(data)) {
+    const tNP = data[0].value
+    console.log(tNP)
+  }
 
-  const tNP = data[0].value
-  console.log(tNP)
 
 }
