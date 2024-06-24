299d058dab92ddade2662ca89cec62d7f2fd3c98
pixeon-sro <box@pixeon.sk>
2024-06-24 17:47:50 +0200

    develop

--- grist/cenex/narezPlan/dataNP.js
+++ grist/cenex/narezPlan/dataNP.js
@@ -44,6 +44,52 @@ function isEmpty(value) {
   }
 }
 
+function createPrintTable(printData, printPlace) {
+
+  //vytvorenie a umiestnenie tabuľky
+  const place=document.getElementById("printPlace")
+  const tab=document.createElement("table")
+  tab.setAttribute("id", printPlace+"_table")
+  place.appendChild(tab)
+
+  const tRow=tab.insertRow(-1)
+  tRow.setAttribute("class", "header")
+  const cellDrevina=tRow.insertCell(0)
+  const cellRezivo=tRow.insertCell(1)
+  const cellVyska=tRow.insertCell(2)
+  const cellSirka=tRow.insertCell(3)
+  const cellDlzka=tRow.insertCell(4)
+  const cellMnozstvo=tRow.insertCell(5)
+  const cellObjem=tRow.insertCell(6)
+
+  cellDrevina="Drevina"
+  cellRezivo="Rezivo"
+  cellVyska="Výška (mm)"
+  cellSirka="Šírka (mm)"
+  cellDlzka="Dĺžka (mm)"
+  cellMnozstvo="Množstvo (ks)"
+  cellObjem="Objem (m3)"
+
+  printData.forEach(function(item) {
+    const itemRow=tab.insertRow(-1)
+    const cellDrevina=itemRow.insertCell(0)
+    const cellRezivo=itemRow.insertCell(1)
+    const cellVyska=itemRow.insertCell(2)
+    const cellSirka=itemRow.insertCell(3)
+    const cellDlzka=itemRow.insertCell(4)
+    const cellMnozstvo=itemRow.insertCell(5)
+    const cellObjem=itemRow.insertCell(6)
+
+    cellDrevina=item.drevina
+    cellRezivo=item.rezivo
+    cellVyska=item.vyska
+    cellSirka=item.sirka
+    cellDlzka=item.dlzka
+    cellMnozstvo=item.mnozstvo
+    cellObjem=item.objem
+  })
+}
+
 // grist požaduje plný prístup
 grist.ready({ requiredAccess: 'full' })
 
@@ -62,7 +108,7 @@ allPromises = [
 
 // načítanie všetkych Promisov a príprava polí objektov
 Promise.allSettled(allPromises).then(function(data){
-  console.log(data)
+
   /*
   /  ak existuje tabuľka Nárezový plán a
   /  obsahuje data, vytvoria sa údaje pre tlač
@@ -70,7 +116,5 @@ Promise.allSettled(allPromises).then(function(data){
   if (!isEmpty(data)) {
     const tNP = data[0].value
     console.log(tNP)
-  }
-
-
+    createPrintTable(tNP, "placeNP")
 })
