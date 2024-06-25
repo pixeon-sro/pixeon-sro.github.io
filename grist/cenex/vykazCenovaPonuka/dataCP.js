ebb58f94bce1fbfd7f45803cb46d4ec0ee99dc6f
pixeon-sro <box@pixeon.sk>
2024-06-25 17:08:03 +0200

    develop

--- grist/cenex/vykazCenovaPonuka/dataCP.js
+++ grist/cenex/vykazCenovaPonuka/dataCP.js
@@ -252,6 +252,16 @@ Promise.allSettled(allPromises).then(function(data){
   cellIbanKey.innerHTML="iBAN:"
   cellIbanValue.innerHTML=tProfile.iban[0]
 
+  const tProfRowE=tabProfile.insertRow(-1)
+  const cellMailKey=tProfRowE.insertCell(0)
+  const cellMailValue=tProfRowE.insertCell(1)
+  const cellTelKey=tProfRowE.insertCell(2)
+  const cellTelValue=tProfRowA.insertCell(3)
+  cellMailKey.innerHTML="E-mail:"
+  cellMailValue.innerHTML=tProfile.mail[0]
+  cellTelKey.innerHTML="Telefón:"
+  cellTelValue.innerHTML=tProfile.telefon[0]
+
   // vypísanie Výkazu Výmer Materiál
   let htmlH1Material = document.getElementById("htmlH1Material")
   let htmlTableMaterial = document.getElementById("htmlTableMaterial");
